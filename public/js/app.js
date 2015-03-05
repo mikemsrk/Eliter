fbf39644e2631edafaa993f4a0b1d61084d68d84
mikemsrk <min2033@gmail.com>
2015-03-04 20:37:21 -0800

    initial backbone app

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
 	console.log('app loaded');
 
+	var Goal = Backbone.Model.extend({
+		defaults:{
+			date: new Date(),
+			text: 'Read a book!'
+		}
+	});
 
+	var Goals = Backbone.Collection.extend({
+		model:Goal
+
+	});
 
 });
